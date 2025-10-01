fa59b3b26ad456136be1647161da34586e3333a6
Ximi Hoque <hoque.ximi@gmail.com>
2025-10-01 19:46:16 +0530

    [LABIMP-7764] Created modules structure and directories

---
+++ labellerr/core/exceptions/__init__.py
@@ -0,0 +1,10 @@
+
+"""
+This module will contain all exceptions for the SDK. We need to define exceptions for Authentication, DataValidation, Support, Rate limits etc.
+
+"""
+
+class LabellerrError(Exception):
+    """Custom exception for Labellerr SDK errors."""
+
+    pass
---
+++ labellerr/core/validators/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module will contain all validators for the SDK. Example - validations like incorrect email format, incorrect data type, etc. 
+Invalid values that should not be accepted by the API go here. Example - local upload file size limit, etc. 
+These validations should be only handle those which can't be captured by the typings.
+"""
