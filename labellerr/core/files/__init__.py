375884552d932c104ffbd27d96fe6d6f09b38a67
yashsuman15 <114386148+yashsuman15@users.noreply.github.com>
2025-10-08 18:54:37 +0530

    minor update

---
+++ labellerr/core/files/__init__.py
@@ -0,0 +1,14 @@
+# Import base classes
+from labellerr.core.files.base import LabellerrFile, LabellerrFileMeta
+
+# Import subclasses to trigger registration
+# These imports register each file type with the metaclass
+from labellerr.core.files.image_file import LabellerrImageFile
+from labellerr.core.files.video_file import LabellerrVideoFile
+
+__all__ = [
+    'LabellerrFile',
+    'LabellerrImageFile', 
+    'LabellerrVideoFile',
+    'LabellerrFileMeta'
+]
